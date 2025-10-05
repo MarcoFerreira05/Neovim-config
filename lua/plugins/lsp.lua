c05237409f245bd8fd878033bf96a9c5c08a34cc
MarcoFerreira05 <a106857@alunos.uminho.pt>
2025-10-05 23:47:46 +0100

    Add Python LSP and Linter+Formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -212,6 +212,23 @@ return {
     		),
 		},
 		bashls = {},
+	    ruff = {},
+		pylsp = {
+			settings = {
+				pylsp = {
+					plugins = {
+						pyflakes = {enabled = false},
+						pycodestyle = {enabled = false},
+						autopep8 = {enabled = false},
+						yapf = {enabled = false},
+						mccabe = {enabled = false},
+						pylsp_mypy = {enabled = false},
+						pylsp_black = {enabled = false},
+						pylsp_isort = {enabled = false}
+					}
+				}
+			}
+		},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
